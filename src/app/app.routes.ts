5ab01d71228adbe457f76c684591c96eaa967edc
hugo-degrossi <contact@hugo-degrossi.fr>
2023-10-19 16:26:23 +0200

    missing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: 'league/:leagueId', loadComponent: () => import('./features/leaderboard/leaderboard.component').then(mod => mod.LeaderboardComponent)},
-    {path: 'league/:leagueId/team/:teamId/history', loadComponent: () => import('./features/team-history/team-history.component').then(mod => mod.TeamHistoryComponent)},
+  {path: 'league/:leagueId', loadComponent: () => import('./features/leaderboard/leaderboard.component').then(mod => mod.LeaderboardComponent)},
+  {path: 'league/:leagueId/team/:teamId/history', loadComponent: () => import('./features/team-history/team-history.component').then(mod => mod.TeamHistoryComponent)},
+  {path: '**', loadComponent: () => import('./app.component').then(mod => mod.AppComponent)},
+
 ];
