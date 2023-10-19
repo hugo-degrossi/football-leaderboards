dcb19962641c94af51706fa707f51176d4294b0d
hugo-degrossi <contact@hugo-degrossi.fr>
2023-10-19 16:30:30 +0200

    build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {path: 'league/:leagueId', loadComponent: () => import('./features/leaderboard/leaderboard.component').then(mod => mod.LeaderboardComponent)},
   {path: 'league/:leagueId/team/:teamId/history', loadComponent: () => import('./features/team-history/team-history.component').then(mod => mod.TeamHistoryComponent)},
-  {path: '**', loadComponent: () => import('./app.component').then(mod => mod.AppComponent)},
+  {path: '**', loadComponent: () => import('./core/components/home/home.component').then(mod => mod.HomeComponent)},
 
 ];
